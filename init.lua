6da7e69717ae858e0c23d6846f00ff0356e8e706
Sal <saleh.shehata@outlook.com>
2023-05-01 13:51:32 -0600

    Add nvim configs.

---
+++ init.lua
@@ -0,0 +1,14 @@
+vim.opt.termguicolors = true
+require('packer_init')
+require('core/options')
+require('core/autocmds')
+require('core/keymaps')
+require('core/colors')
+require('core/statusline')
+require('lsp/lspconfig')
+require('plugins/nvim-tree')
+require('plugins/indent-blankline')
+require('plugins/nvim-cmp')
+require('plugins/nvim-treesitter')
+require('plugins/alpha-nvim')
+
